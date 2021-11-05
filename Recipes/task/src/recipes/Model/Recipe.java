a34620c30f76720f81b51cd4367c6caca7d4802e
Hyun Ouk Nam <hyunouk.nam@gmail.com>
2021-11-05 11:25:28 -0400

    removed lombok and added id field to Recipe class, added getters/setters

--- Recipes/task/src/recipes/Model/Recipe.java
+++ Recipes/task/src/recipes/Model/Recipe.java
@@ -1,15 +1,52 @@
 package recipes.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Recipe {
+    String id;
     String name;
     String description;
-    String ingredients;
-    String directions;
+    String[] ingredients;
+    String[] directions;
+
+    public Recipe() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String[] getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String[] directions) {
+        this.directions = directions;
+    }
 }
